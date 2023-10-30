2a3f9a2e357cba6d122c7f0d2490d5ab4eec3c36
lajesfen <luciano.aguirre@utec.edu.pe>
2023-10-30 16:42:28 -0500

    add: files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import 'devextreme/dist/css/dx.light.css';
+import { DataGrid } from 'devextreme-react/data-grid';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8080/groups');
+      const data = await response.json();
+
+      console.log(data);
+      return (
+        <div>
+          <DataGrid id="dataGrid" dataSource={data}>
+          </DataGrid>
+        </div>
+      );
+
+    } catch (error) {
+      console.log('Failed to fetch');
+    }
+  }
+
+  fetchData();
 }
 
 export default App;
